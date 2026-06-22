4a2adfb016d6ecdecf45ebb03ff3dc38e29c96c1
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2026-06-22 13:37:06 +0530

    day_02

---
+++ DayTask_02/src/Components/Task_01.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+const Task_01 = () => {
+  return (
+    <>
+      <h1>
+        Create 4 react app and write some code in it with using some tags like
+        ul,ol paragraph etc..
+      </h1>
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+      </ul>
+      <ol>
+        <li>python</li>
+        <li>nodejs</li>
+        <li>reactjs</li>
+      </ol>
+      <p>asdfghjkl;' dfghjkl;cvbnm,qewertyuiop[]dxfcvbnmm</p>
+    </>
+  );
+};
+export default Task_01;
