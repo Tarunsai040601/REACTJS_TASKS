e097a2fe6956f74ba5e86df51b3a4c5ef319e469
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2026-07-13 11:25:31 +0530

    added

---
+++ DayTask_06/src/Task_01/Task_01.jsx
@@ -0,0 +1,18 @@
+import { React } from "react";
+import butterfly from "../../public/butterfly.jpg";
+export function Task_01() {
+  return (
+    <>
+      <img src={butterfly} alt="butterimage" height={"500px"} />
+    </>
+  );
+}
+
+
+export function video(){
+    return (
+    <>
+      <img src={butterfly} alt="butterimage" height={"500px"} />
+    </>
+  );
+}
